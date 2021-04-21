7de6e8d9d9671f7419f8799554fc7f914b3edfc8
kadismile <brianking319@gmail.com>
2021-04-21 12:20:02 +0100

    refactored application

---
+++ build/precache-manifest.249d255fe7db994785ce7f17cc4f652c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f24b5cc81fec651fa1807a740dcc824",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f56e889cb74060871e7",
+    "url": "/static/css/3.00129a64.chunk.css"
+  },
+  {
+    "revision": "786c6b7816fde6af6a73",
+    "url": "/static/js/2.34151b1e.chunk.js"
+  },
+  {
+    "revision": "0ac173ce5a3cc2df9ee2e483720eb73b",
+    "url": "/static/js/2.34151b1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f56e889cb74060871e7",
+    "url": "/static/js/3.77ba449a.chunk.js"
+  },
+  {
+    "revision": "80e109d37ee38a0b1d525ee1bdf87c21",
+    "url": "/static/js/3.77ba449a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8516ce002b3010d7583",
+    "url": "/static/js/4.ba90e59e.chunk.js"
+  },
+  {
+    "revision": "0ac173ce5a3cc2df9ee2e483720eb73b",
+    "url": "/static/js/4.ba90e59e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0652fd6aee3ef5f4430e",
+    "url": "/static/js/5.f7f97a47.chunk.js"
+  },
+  {
+    "revision": "d6e8013b304717714a45",
+    "url": "/static/js/main.bb666090.chunk.js"
+  },
+  {
+    "revision": "175dee4c36cc200948d3",
+    "url": "/static/js/runtime-main.a252725c.js"
+  }
+]);
